8ea194d0d198065903686b7015589c7f4527fdf1
Brayan Eduardo Tepas Linares <tl25001@esfe.agape.edu.sv>
2025-05-30 14:54:31 -0600

    ejercicio de contar palabras terminado

--- ejercicio1contarpalabras/Main.java
+++ ejercicio1contarpalabras/Main.java
@@ -1,3 +1,20 @@
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Por favor, ingrese un texto para contar las palabras:");
+        String texto = scanner.nextLine();
+        if (texto.isEmpty()) {
+            System.out.println("El texto está vacío. No se pueden contar palabras.");
+            
+        }else{
+            String[] palabras = texto.trim().split("\\s+");
+            int contador = palabras.length;
+            System.out.println("El número de palabras en el texto es: " + contador);
+        } 
+        scanner.close();
+    }
 
 }
